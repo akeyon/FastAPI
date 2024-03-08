0ea3a5f8002cbbd0bc0d26e21b4d1a2db1ca6d06
akeyon <onorah17@gmail.com>
2024-03-08 17:02:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ app = FastAPI()
 class Msg(BaseModel):
     msg: str
 
-
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+    
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+    
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World edited!", "message": "Welcome to FastAPI!"}
@@ -14,6 +23,8 @@ async def root():
 @app.get("/path")
 async def demo_get():
     return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+
+
     
 
 
