bbbc41487b8e3844b88fd3abcf763485071c20c9
akeyon <onorah17@gmail.com>
2024-03-08 16:40:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World edited!", "message": "Welcome to FastAPI!"}
+
+@app.get("/path")
+async def demo_get():
+    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
